3c7d0f561114487041ab547d7d2a0aa4fccb0b74
Navrose Singh Johal <142087886+RozForCode@users.noreply.github.com>
2025-01-26 17:04:13 -0500

    Update Card.java

--- src/card/Card.java
+++ src/card/Card.java
@@ -11,6 +11,20 @@ package card;
  * There are 52 cards in a deck, no jokers.
  * This code is to be used in ICE1. When you create your own branch,
  * add your name as a modifier.
+ * 
+ * Modified by: Navrose Johal
+ * Student ID: 123456789
+ * Date: January 26, 2025
+ * 
+ 
+ */
+/**
+ * A class that models playing card Objects. Cards have 
+ * a value (note that Ace = 1, Jack -11, Queen =12, King = 13)
+ * A suit (clubs, hearts, spades, diamonds).
+ * There are 52 cards in a deck, no jokers.
+ * This code is to be used in ICE1. When you create your own branch,
+ * add your name as a modifier.
  * @author srinivsi
  */
 public class Card {
